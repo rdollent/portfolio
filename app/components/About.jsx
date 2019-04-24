06884b1fb5a94b2584485817048e297f190956ec
Rod <rumbalumba89@gmail.com>
2019-04-24 19:43:31 +0000

    added About Me

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,8 @@ class About extends React.Component {
         return (
             <div id='about'>
                 <h3><strong>ABOUT ME</strong></h3>
-                <p>Self-taught individual looking to get into web development.</p>
+                <p>Self-taught individual looking to get into web development. After being exposed to VBA and SQL, I decided to learn programming and chose web dev. I picked up John Duckett's introductory HTML/CSS book and went on from there. I have since gone on to learn about full-stack technologies through online resources. I continue to learn more and currently am a part of Chingu, an online cohort for people to build web dev projects together remotely.
+                </p>
             </div>
         )
     }
