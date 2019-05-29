21eaeb2a4ff70dae338e33e61342f70db925248e
Rod <rumbalumba89@gmail.com>
2019-05-29 21:05:14 +0000

    edited project descriptions and added github link

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,7 @@ class About extends React.Component {
         return (
             <div id='about'>
                 <h3><strong>ABOUT ME</strong></h3>
-                <p>Self-taught individual looking to get into web development. After being exposed to VBA and SQL, I decided to learn programming and chose web dev. I picked up John Duckett's introductory HTML/CSS book and went on from there. I have since gone on to learn about full-stack technologies through online resources. I continue to learn more and currently am a part of Chingu, an online cohort for people to build web dev projects together remotely.
+                <p>Self-taught individual looking to get into web development. After being exposed to VBA and SQL, I decided to learn programming and chose web dev. I picked up John Duckett's introductory HTML/CSS book and went on from there. I have since gone on to learn about full-stack technologies through online resources. I continue to learn more and currently am a part of Chingu, an online cohort for people to build web dev projects together remotely. Please check out my <a href='https://github.com/rdollent' target='_blank'>Github profile</a>.
                 </p>
             </div>
         )
--- app/objects/projects.js
+++ app/objects/projects.js
@@ -2,7 +2,7 @@
 
 const calc     = {
         img: require('../assets/projects/portf-calc.png'),
-        desc: 'Calculator built using Javascript',
+        desc: 'Calculator built using JavaScript',
         url: 'https://rdollent.github.io/FCC_Calculator/',
         github: 'https://github.com/rdollent/FCC_Calculator' 
         };
@@ -14,25 +14,25 @@ const clock    = {
         };
 const gaming   = {
         img: require('../assets/projects/portf-gaming.png'),
-        desc: 'Video game query using GiantBomb API',
+        desc: 'Video game search using Giantbomb API, JS',
         url: 'https://rdollent.github.io/GamingSite/',
         github: 'https://github.com/rdollent/GamingSite' 
         };
 const drums    =  {
         img: require('../assets/projects/portf-jsdrums.png'),
-        desc: 'Drum sounds with each key press',
+        desc: 'Drum sounds with each key press built on JS',
         url: 'https://rdollent.github.io/JS30_Drums/',
         github: 'https://github.com/rdollent/JS30_Drums' 
         };
 const patatap  = {
         img: require('../assets/projects/portf-patatapclone.png'),
-        desc: 'Patatap clone app exercise',
+        desc: 'Patatap clone app exercise using JS',
         url: 'https://rdollent.github.io/Patatap_Clone/',
         github: 'https://github.com/rdollent/Patatap_Clone' 
         };
 const pomodoro = {
         img: require('../assets/projects/portf-pomodoro.png'),
-        desc: 'Pomodoro timer',
+        desc: 'FreeCodeCamp\'s  Pomodoro timer challenge',
         url: 'https://rdollent.github.io/FCC_Pomodoro/',
         github: 'https://github.com/rdollent/FCC_Pomodoro' 
         };
@@ -44,25 +44,25 @@ const quote    = {
         };
 const books    = {
         img: require('../assets/projects/portf-react-books.png'),
-        desc: 'Book search using React and Google Books API',
+        desc: 'Book search with React, Redux and Google Books API',
         url: 'https://jun-chingu-book-finder.herokuapp.com/',
         github: 'https://github.com/rdollent/chingu-solo-project' 
         };
 const timer    = {
         img: require('../assets/projects/portf-react-timer.png'),
-        desc: 'Timer built on React',
+        desc: 'Timer built on React (Udemy course)',
         url: 'https://infinite-journey-55654.herokuapp.com/',
         github: 'https://github.com/rdollent/ReactTimerApp' 
         };
 const rtodo    = {
         img: require('../assets/projects/portf-react-todo.png'),
-        desc: 'Todo list built on React',
+        desc: 'Todo list built on React (Udemy course)',
         url: 'https://rdollent-react-todo.herokuapp.com/',
         github: 'https://github.com/rdollent/react-todo' 
         };
 const todo     = {
         img: require('../assets/projects/portf-todo.png'),
-        desc: 'Todo list inspired by Google Calendar',
+        desc: 'Google Calendar-esque todo app using HTML, CSS, JavaScript',
         url: 'https://rdollent-todo.herokuapp.com/',
         github: 'https://github.com/rdollent/todolist' 
         };
@@ -80,7 +80,7 @@ const weather2 = {
         };
 const weather  = {
         img: require('../assets/projects/portf-weather.png'),
-        desc: 'Weather app built on native JS',
+        desc: 'Weather app built on vanilla JavaScript',
         url: 'https://rdollent.github.io/FCC_Weather/',
         github: 'https://github.com/rdollent/FCC_Weather' 
         };
@@ -92,27 +92,27 @@ const wiki     = {
         };
 const yelpcamp = {
         img: require('../assets/projects/portf-yelpcamp.png'),
-        desc: 'Camping grounds gallery',
+        desc: 'Camping grounds gallery (Udemy course)',
         url: 'https://desolate-stream-87694.herokuapp.com/',
         github: 'https://github.com/rdollent/yelpcamp/tree/master/YelpCamp/v13' 
         };
 
 const kachingu = {
         img: require('../assets/projects/portf-tracker.png'),
-        desc: 'Expense Tracker remote team project in React, PostgresSQL, Node',
+        desc: 'Expense Tracker remote team project in React, PostgresSQL, Node, Express',
         url: 'https://ka-chingu.herokuapp.com/',
         github: 'https://github.com/chingu-voyages/v8-bears-team-13' 
         };
         
 const meteor = {
         img: require('../assets/projects/portf-meteor.png'),
-        desc: 'NASA meteor directory lookup in React/Redux',
+        desc: 'NASA meteor directory API lookup in React/Redux',
         url: 'https://jun-nasa-meteor.herokuapp.com/',
         github: 'https://github.com/rdollent/NASAMeteor' 
         };
 
 
-const Obj = [kachingu, todo, meteor, books, calc, clock, gaming, drums, patatap, pomodoro, quote, timer, rtodo, tribute, weather2, weather, wiki, yelpcamp];
+const Obj = [kachingu, todo, meteor, books, gaming, calc, clock, drums, patatap, pomodoro, quote, timer, rtodo, tribute, weather2, weather, wiki, yelpcamp];
 
 //patatap, pomodoro, quote, books, timer, rtodo, todo, tribute, weather2, weather, wiki, yelpcamp
 
